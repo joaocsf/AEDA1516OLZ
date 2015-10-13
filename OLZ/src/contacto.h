4d5c17690afaaf8e5d88ad4e0bfc803864007b9d
Joao Ferreira <noobcsf@hotmail.com>
2015-10-13 13:45:46 +0100

    Alterada variavel visiveis de vetor<string> para vetor<string*>

--- OLZ/src/contacto.h
+++ OLZ/src/contacto.h
@@ -11,7 +11,7 @@ class Contacto{
 	string email;
 	string telefone;
 	string nome;
-	vector<string> visiveis; //podemos por aqui os dados que são visiveis e quando queremos fazer display fazemos display do vetor
+	vector<string*> visiveis; //podemos por aqui os dados que são visiveis e quando queremos fazer display fazemos display do vetor
 public:
 
 
