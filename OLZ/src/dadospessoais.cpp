7bfd06f1ba38e6750e4b7ce25326b33a5cadd72c
Joao Ferreira <noobcsf@hotmail.com>
2015-10-24 18:24:50 +0100

    Adeichionhadas foxões nechechárias aue funhecionhemenheto dues dadnhos pechoais.

---
+++ OLZ/src/dadospessoais.cpp
@@ -0,0 +1,52 @@
+#include "dadospessoais.h"
+
+DadosPessoais::DadosPessoais(string nome, string telefone, string email) {
+	this->nome = nome;
+	this->telefone = telefone;
+	this->email = email;
+}
+
+void DadosPessoais::alterarNome(string novoNome) {
+	nome = novoNome;
+}
+
+void DadosPessoais::alterarTelefone(string novoTelefone) {
+	telefone = novoTelefone;
+}
+
+void DadosPessoais::alterarEmail(string novoEmail) {
+	email = novoEmail;
+}
+
+void DadosPessoais::visibilidadeEmail(bool visivel) {
+	visiveis[VisibilidadeDados::EMAIL] = visivel;
+}
+
+void DadosPessoais::visibilidadeNome(bool visivel) {
+	visiveis[VisibilidadeDados::NOME] = visivel;
+}
+
+void DadosPessoais::visibilidadeTelefone(bool visivel) {
+	visiveis[VisibilidadeDados::TELEFONE] = visivel;
+}
+
+string DadosPessoais::getInfo() const {
+	string info;
+	for (int i = 0; i < VisibilidadeDados::TOTAL; ++i) {
+		if (visiveis[i]) {
+			switch (i) {
+			case VisibilidadeDados::NOME:
+				info += nome + "\n";
+				break;
+			case VisibilidadeDados::TELEFONE:
+				info += telefone + "\n";
+				break;
+			case VisibilidadeDados::EMAIL:
+				info += email + "\n";
+				break;
+			}
+		}
+	}
+	return info;
+}
+
--- OLZ/src/dadospessoais.h
+++ OLZ/src/dadospessoais.h
@@ -1,22 +1,54 @@
 #ifndef DADOSPESSOAIS_H_
 #define DADOSPESSOAIS_H_
 
-
-
 #include "utilidades.h"
 
-
-class DadosPessoais{
-	string email;
-	string telefone;
-	string nome;
-	vector<string*> visiveis; //podemos por aqui os dados que são visiveis e quando queremos fazer display fazemos display do vetor
-public:
-
-
-
+enum VisibilidadeDados{
+	NOME,
+	TELEFONE,
+	EMAIL,
+	TOTAL
 };
 
+class DadosPessoais {
 
+	string nome;
+	string telefone;
+	string email;
+
+	bool visiveis[VisibilidadeDados::TOTAL]; //podemos por aqui os dados que são visiveis e quando queremos fazer display fazemos display do vetor
+public:
+
+	DadosPessoais(string nome, string telefone, string email);
+
+	/* "Altera 'O' nome" by Martins
+	 */
+	void alterarNome(string novoNome);
+
+	/* "Altera 'O' telefone" by Martins
+	 */
+	void alterarTelefone(string novoTelefone);
+
+	/* "Altera 'O' e-mail" by Martins
+	 */
+	void alterarEmail(string novoEmail);
+
+	/*Define a visibilidade do email.
+	 */
+	void visibilidadeEmail(bool visivel);
+
+	/*Define a visibilidade do Nome.
+	 */
+	void visibilidadeNome(bool visivel);
+
+	/*Define a visibilidade do Telefone
+	 */
+	void visibilidadeTelefone(bool visivel);
+	/*Devolve informacao visivel
+	 */
+
+	string getInfo() const;
+
+};
 
 #endif
