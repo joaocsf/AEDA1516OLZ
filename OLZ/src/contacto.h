ae57ae6d1e4d0b0e87a2ba2c0e31de816933ce4a
Joao Ferreira <noobcsf@hotmail.com>
2015-10-24 14:56:15 +0100

    UpdateContacto

--- OLZ/src/contacto.h
+++ OLZ/src/contacto.h
@@ -5,15 +5,18 @@
 #include <vector>
 #include <string>
 
-using namespace std;
 
 class Contacto{
-	string email;
-	string telefone;
-	string nome;
-	vector<string*> visiveis; //podemos por aqui os dados que são visiveis e quando queremos fazer display fazemos display do vetor
-public:
+	std::string _nome;
+	std::string _email;
+	std::string _telefone;
 
+	vector<std::string*> _visiveis; //podemos por aqui os dados que são visiveis e quando queremos fazer display fazemos display do vetor (Ocupar 2x espaco na memoria?)
+	//bool _visiveis[3]; < sempre poupamos na memoria
+public:
+	Contacto(std::string nome,std::string mail, std::string ntelefone);
+	//Mostra todas as Informacoes visiveis do utilizador
+	std::string ObterInfo();
 
 
 };
