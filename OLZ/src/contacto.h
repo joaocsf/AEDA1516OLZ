208bcbea732017f1af6b29ead1aa0df1a449389e
Joao Ferreira <noobcsf@hotmail.com>
2015-10-24 15:06:42 +0100

    comentario

---
+++ OLZ/src/contacto.h
@@ -0,0 +1,22 @@
+#ifndef CONTACTO_H_
+#define CONTACTO_H_
+
+#include "utilidades.h"
+#include "dadospessoais.h"
+
+
+class Contacto{
+
+	string mensagem;
+	DadosPessoais dados;
+public:
+	Contacto(std::string nome,std::string mail, std::string ntelefone);
+	//Mostra todas as Informacoes visiveis do utilizador
+	std::string ObterInfo();
+
+
+};
+
+
+
+#endif
