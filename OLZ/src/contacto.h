8403047f0f9ab2b0267401295b2458bd186c9d94
David <davidazevedo1996@hotmail.com>
2015-11-01 17:23:25 +0000

    Sexo é hereditário se os teus pais não fizeram é provavel que também não vás fazer.

--- OLZ/src/contacto.h
+++ OLZ/src/contacto.h
@@ -5,17 +5,40 @@
 #include "dadospessoais.h"
 #include "dados.h"
 
-
+//!  Classe Contacto.
+/*!
+  Esta classe contêm as informaçoes relativas ao Utilizador que criou um anuncio nomeadamente a forma de contacto por parte dos interessados.
+*/
 class Contacto: public Dados{
-
+	//! Mensagem.
+	    /*!
+	      Mensagem a ser enviada ao dono do anuncio.
+	    */
 	string _mensagem;
+	//! Dados.
+	    /*!
+	      Contem os dados pessoais do anunciante que vai receber/enviar a mensagem.
+	    */
 	DadosPessoais _dados;
 public:
-	Contacto();
-	Contacto(string mensagem, DadosPessoais dados);
-	string getInfo()const;
-	void setMensagem(string mensagem);
 
+	Contacto();
+	//! Overload do Construtor que recebe dois argumentos.
+	    /*!
+	      \param mensagem a mensagem a ser enviada.
+	      \param dados os dados do utilizador que vai receber/enviar a mensagem.
+	    */
+	Contacto(string mensagem, DadosPessoais dados);
+	//! Funcao que retorna a informaçao (mensagem + dados pessoais).
+	    /*!
+	      \return O mensagem juntamente com os dados.
+	    */
+	string getInfo()const;
+	//! Funcao que altera a variavel mensagem.
+	    /*!
+	     *
+	    */
+	void setMensagem(string mensagem);
 	virtual void ler(ifstream& in,bool escreve= false);
 	virtual void escrever(ofstream& out);
 
