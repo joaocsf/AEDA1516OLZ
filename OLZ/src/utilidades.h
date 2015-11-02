a74f56f3e312922056a2e28e14dee4a0f37e1f9b
David <davidazevedo1996@hotmail.com>
2015-11-02 16:03:39 +0000

    quando a noite cai elas saem as ruas, com belos vestidos com transparente para mostrarem as mamas

--- OLZ/src/utilidades.h
+++ OLZ/src/utilidades.h
@@ -17,20 +17,34 @@ void intro();
 
 
 
-
+//! Estrutura localizacao.
+/*!
+ *Serve para organizaçao a localizacao de cada utilizador
+ */
 struct Localizacao{
+	//! Uma variavel publica.
 	string freguesia;
+	//! Uma variavel publica.
 	string concelho;
+	//! Uma variavel publica.
 	string distrito;
 };
 
+//! Estrutura Imagem.
 struct Imagem{
 	string conteudo;
 };
 
-
+//! Funcao que altera a cor das letras que irão ser impressas na consola.
+	    /*!
+	      \param color a cor para a qual vai alterar.
+	    */
 void setcolor(unsigned int color);
-
+//! Funcao que altera a cor das letras que irão ser impressas na consola e o fundo.
+	    /*!
+	      \param color a cor para a qual se vai alterar as letras.
+	      \param background_color a cor para a qual se vai alterar o fundo.
+	    */
 void setcolor(unsigned int color, unsigned int background_color);
 
 
