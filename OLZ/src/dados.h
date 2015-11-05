aaaf2286f5b5f40e979c88b96372d3f6742fff51
David <davidazevedo1996@hotmail.com>
2015-11-05 11:23:17 +0000

    and another one bites the dust

--- OLZ/src/dados.h
+++ OLZ/src/dados.h
@@ -14,24 +14,43 @@ using namespace std;
 
 //! Class Dados
 /*!
- * Classe mae que trata de todo o tipo de dados relativos aos ficheiros (Leitura e Escrita).
+ * Classe mae que trata de todo o tipo de dados. Esta classe contem as declaracoes dos membros e funcoes do programa principal.
  */
 class Dados{
 protected:
+	//!Declaracao global do vetor de anuncios do programa.
+	/*!
+	 \sa Anuncio
+	 */
 	static vector<Anuncio *>* vetorAnuncios;
+	//!Declaracao global do vetor de utilizadores do programa.
+	/*!
+	 \sa Utilizador
+	 */
 	static vector<Utilizador*>* vetorUtilizadores;
+	//!Declaracao global do vetor de negocios do programa.
+	/*!
+	 \sa Negocio
+	 */
 	static vector<Negocio* >* vetorNegocios;
 public:
-
+	//!Funcao set para o vetor de anuncios.
 	static void setVetorAnuncio(vector<Anuncio*>* anuncios);
+	//!Funcao set para o vetor de utilizadores.
 	static void setVetorUtilizadores(vector<Utilizador*>* utilizadores);
+	//!Funcao set para o vetor de negocios.
 	static void setVetorNegocios(vector<Negocio*>* negocios);
 
+	//!Funcao get para o vetor de anuncios.
 	static vector<Anuncio*>* getVetorAnuncio();
+	//!Funcao get para o vetor de utilizadores.
 	static vector<Utilizador*>* getVetorUtilizadores();
+	//!Funcao get para o vetor de negocios.
 	static vector<Negocio*>* getVetorNegocios();
 
+	//!Funcao para a leitura de toda a informacao contida no ficheiro de dados relativos ao programa.
 	static void lerFicheiro(ifstream& in);
+	//!Funcao para a escrito de toda a informacao para o ficheiro de dados relativos ao programa.
 	static void escreverFicheiro(ofstream& out);
 
 
